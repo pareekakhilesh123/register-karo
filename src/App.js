8dd0bb14bcb6efe6ede05f593b92104b0ae12b96
pareekakhilesh123 <pareekakhilesh5@gmail.com>
2025-01-21 19:35:09 +0530

    Project SetUp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ 
+import './App.css';
+import HeroSection from './assets/components/HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+    <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/components/HeroSection.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import "../styles/HeroSection.css"; 
+const HeroSection = () => {
+  return (
+    <>
+      <section className="hero-section">
+        <div className="container">
+          <h1>Your Trusted Partner for Compliance Business Needs</h1>
+          <p>Providing end-to-end solutions...</p>
+          <button className="btn btn-primary">Visit Us</button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
