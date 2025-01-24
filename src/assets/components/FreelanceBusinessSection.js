fc2985472b452ed2d798fdf806b25c23e1b5b1e9
pareekakhilesh123 <pareekakhilesh5@gmail.com>
2025-01-24 13:41:27 +0530

    ...

--- src/assets/components/FreelanceBusinessSection.js
+++ src/assets/components/FreelanceBusinessSection.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import '../styles/Sectionbackground.css'
+
 import logoimg from "../images/FreelancebBusinessSection/Logos.webp"
 
 function FreelanceBusinessSection() {
   return (
-    <div className='Sectionbackground'>
-        <h2>Trusted By Over 100+ Startups and freelance business</h2>
+    <div style={{width:"100%" , height:"240px"}}>
+        <h2 className='p-5'>Trusted By Over 100+ Startups and freelance business</h2>
         <img src={logoimg}
             alt=' Startups logo'
             width={"100%"}
