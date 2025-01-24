392a1e905e3daf004835ec6933e2d46b0dd376b5
pareekakhilesh123 <pareekakhilesh5@gmail.com>
2025-01-24 11:36:42 +0530

    Some Change

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
+import React, { Suspense } from "react";
  
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import HeroSection from './assets/components/HeroSection';
-import Header from './assets/components/Header';
-import FreelanceBusinessSection from './assets/components/FreelanceBusinessSection';
-import Services from './assets/components/Services';
-import About from './assets/components/About';
-import WhyRegister from './assets/components/WhyRegister';
-import VideoIntroduction from './assets/components/VideoIntroduction';
-import OurHappyClinet from './assets/components/OurHappyClinet';
-import Process from './assets/components/Process';
-import BlogLayout from './assets/components/BlogLayout';
-import TestimonialsSection from './assets/components/TestimonialsSection';
-import FAQ from './assets/components/FAQ';
-import MobileAppServices from './assets/components/MobileAppServices';
-import SomeRegisterNo from './assets/components/SomeRegisterNo';
-import SubscriptionForm from './assets/components/SubscriptionForm';
-import Footer from './assets/components/Footer';
+ 
+const HeroSection = React.lazy(() => import("./assets/components/HeroSection"));
+const Header = React.lazy(() => import("./assets/components/Header"));
+const FreelanceBusinessSection = React.lazy(() =>
+  import("./assets/components/FreelanceBusinessSection")
+);
+const Services = React.lazy(() => import("./assets/components/Services"));
+const About = React.lazy(() => import("./assets/components/About"));
+const WhyRegister = React.lazy(() => import("./assets/components/WhyRegister"));
+const VideoIntroduction = React.lazy(() =>
+  import("./assets/components/VideoIntroduction")
+);
+const OurHappyClinet = React.lazy(() =>
+  import("./assets/components/OurHappyClinet")
+);
+const Process = React.lazy(() => import("./assets/components/Process"));
+const BlogLayout = React.lazy(() => import("./assets/components/BlogLayout"));
+const TestimonialsSection = React.lazy(() =>
+  import("./assets/components/TestimonialsSection")
+);
+const FAQ = React.lazy(() => import("./assets/components/FAQ"));
+const MobileAppServices = React.lazy(() =>
+  import("./assets/components/MobileAppServices")
+);
+const SomeRegisterNo = React.lazy(() =>
+  import("./assets/components/SomeRegisterNo")
+);
+const SubscriptionForm = React.lazy(() =>
+  import("./assets/components/SubscriptionForm")
+);
+const Footer = React.lazy(() => import("./assets/components/Footer"));
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    <HeroSection />
-    <FreelanceBusinessSection/>
-    <Services />
-    <About />
-    <WhyRegister />
-    <VideoIntroduction />
-    <OurHappyClinet />
-    <Process />
-    <BlogLayout />
-    <TestimonialsSection />
-    <FAQ />
-    <MobileAppServices />
-    <SomeRegisterNo />
-   <SubscriptionForm /> 
-   <Footer/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <HeroSection />
+        <FreelanceBusinessSection />
+        <Services />
+        <About />
+        <WhyRegister />
+        <VideoIntroduction />
+        <OurHappyClinet />
+        <Process />
+        <BlogLayout />
+        <TestimonialsSection />
+        <FAQ />
+        <MobileAppServices />
+        <SomeRegisterNo />
+        <SubscriptionForm />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
